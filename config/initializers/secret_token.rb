d6c1201fdca574aebf7a36bddbf7d09ed238039a
Kathleen Schadeck <kschadeck@gmail.com>
2012-02-21 21:41:16 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScaffoldingBookmarks::Application.config.secret_token = 'dd17c6f0599a152c1c4e617bedebcdede452742f91bbe471434fccba2c4fc530e27cfec437ef066d8d7d522923b76051681b570f15b649481203b687cde1ea70'
